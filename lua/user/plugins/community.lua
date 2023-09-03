d3e744e9917eebd0702c4d072776c2964478b4c0
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2023-09-03 21:24:57 +0200

    Setup docker

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.bash" },
