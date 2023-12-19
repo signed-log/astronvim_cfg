1bb450763ea0f30f3aa4a7477a802ebf915b93dc
Restyled.io <commits@restyled.io>
2023-12-19 07:20:31 +0000

    Restyled by luaformatter

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -1,14 +1,14 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  { import = "astrocommunity.pack.ansible" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.programming-language-support.csv-vim" },
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+    -- Add the community repository of plugin specifications
+    "AstroNvim/astrocommunity",
+    -- example of imporing a plugin, comment out to use it or add your own
+    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+    {import = "astrocommunity.pack.ansible"},
+    {import = "astrocommunity.pack.docker"},
+    {import = "astrocommunity.pack.python"},
+    {import = "astrocommunity.pack.rust"},
+    {import = "astrocommunity.pack.bash"},
+    {import = "astrocommunity.programming-language-support.csv-vim"}
+    -- { import = "astrocommunity.colorscheme.catppuccin" },
+    -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
